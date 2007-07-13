7f01b2977cc87bae08c50c3eecf962798bd59fc8
mleventi <mleventi@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-07-13 20:54:31 +0000

    *** empty log message ***
    git-svn-id: http://svn.webwork.maa.org/system/trunk/ww_question_server@5172 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Constants.pm
+++ lib/WeBWorK/Constants.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/Constants.pm,v 1.52 2006/11/06 22:22:51 sh002i Exp $
+# $CVSHeader: ww_question_server/lib/WeBWorK/Constants.pm,v 1.1 2007/07/13 20:52:28 mleventi Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -67,4 +67,8 @@ EOF
 $WeBWorK::PG::ImageGenerator::TexPostamble = <<'EOF';
 \end{document}
 EOF
+
+1;
+
+
 
