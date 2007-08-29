fcf7eba042c1e5c518d22a80895a89f201f390af
mleventi <mleventi@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-08-29 04:33:24 +0000

    New version of Server
    git-svn-id: http://svn.webwork.maa.org/system/trunk/ww_question_server@5448 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/ProblemServer/GeneratorRequest.pm
@@ -0,0 +1,16 @@
+package ProblemServer::GeneratorRequest;
+
+=pod
+=begin WSDL
+        _ATTR trials $string The number of attempts at generation
+        _ATTR problem $ProblemServer::ProblemRequest The problem to be generated
+=end WSDL
+=cut
+sub new {
+    my $self = shift;
+    $self={};
+    bless $self;
+    return $self;
+}
+
+1;
